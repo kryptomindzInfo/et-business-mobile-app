eb5b0c845baccfcdb56a4016e405e99ccf9d648f
Faisal Khan <primekhan555@gmail.com>
2024-03-05 09:19:15 +0500

    configuration done

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,39 @@
+import 'package:etbank_business_app/providers/localization_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'navigation/navigator_key.dart';
 import 'navigation/route_generator.dart';
 import 'presentation/views/home_screen.dart';
+import 'resources/localization/app_localization.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const ProviderScope(child: MyApp()));
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends ConsumerWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return MaterialApp(
       title: 'ET Bank Business',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-       navigatorKey: navigatorKey,
+      supportedLocales: AppLocalization.supportedLocales,
+      locale: ref.watch(localizationProvider).locale,
+      localizationsDelegates: const [
+        AppLocalization.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate
+      ],
+      navigatorKey: navigatorKey,
       onGenerateRoute: RouteGenerator.generateRoute,
       initialRoute: HomeScreen.routeName,
     );
   }
 }
-
---
+++ lib/providers/localization_provider.dart
@@ -0,0 +1,41 @@
+// ignore_for_file: depend_on_referenced_packages
+
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+// import 'package:hive/hive.dart';
+
+import '../constants/app_boxes.dart';
+import '../constants/lang_constants.dart';
+import '../api_resources/api_models/language_model.dart';
+
+final localizationProvider = ChangeNotifierProvider<LocalizationProvider>(
+    (ref) => LocalizationProvider());
+
+class LocalizationProvider extends ChangeNotifier {
+  LocalizationProvider() {
+    _loadCurrentLanguage();
+  }
+  // final Box _hive = Hive.box(AppBoxes.appBox);
+  int _languageIndex = 0;
+  Locale _locale = Locale(LangConstants.languages[0].languageCode,
+      LangConstants.languages[0].countryCode);
+
+  Locale get locale => _locale;
+  int get languageIndex => _languageIndex;
+
+  void _loadCurrentLanguage() async {
+    _languageIndex = 0;
+    // _hive.get(LangConstants.languageIndex, defaultValue: _languageIndex);
+    LanguageModel lang = LangConstants.languages.elementAt(_languageIndex);
+    _locale = Locale(lang.languageCode, lang.countryCode);
+    notifyListeners();
+  }
+
+  void setLanguage(int langIndex) {
+    LanguageModel lang = LangConstants.languages.elementAt(langIndex);
+    _locale = Locale(lang.languageCode, lang.countryCode);
+    // _hive.put(LangConstants.languageIndex, langIndex);
+    _languageIndex = langIndex;
+    notifyListeners();
+  }
+}
