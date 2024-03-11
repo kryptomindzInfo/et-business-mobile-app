b60899fee22410a77ffa2d396211c85126ef2a50
Aafaqkhan <itsafaqkhan@gmail.com>
2024-03-11 11:19:21 +0500

    changes

--- lib/presentation/views/signup_screens/signup_widgets/business_type_widget.dart
+++ lib/presentation/views/signup_screens/signup_widgets/business_type_widget.dart
@@ -9,47 +9,57 @@ class BusinessTypeWidget extends ConsumerWidget {
   final String image;
   final String title;
   final String description;
-  VoidCallback? onPressed;
-  BusinessTypeWidget(
+  final VoidCallback? onPressed;
+  final String? labelText;
+  const BusinessTypeWidget(
       {super.key,
       required this.image,
       required this.title,
       required this.description,
-      this.onPressed});
+      this.onPressed,
+      this.labelText});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return InkWell(
       onTap: onPressed,
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.start,
+      child: Column(
         children: [
-          Image.asset(
-            image,
-            height: 34.h,
-            width: 34.w,
+          Text(
+            labelText ?? 'country',
+            style: AppTextstyle.bodyTextStyle(color: AppColors.grey),
           ),
-          16.spaceX,
-          Column(
+          Row(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Text(
-                title,
-                style: AppTextstyle.headingTextStyle(
-                    fontSize: 16.sp,
-                    color: AppColors.white,
-                    fontWeight: FontWeight.w600),
+              Image.asset(
+                image,
+                height: 34.h,
+                width: 34.w,
               ),
-              SizedBox(
-                width: 198.w,
-                child: Text(
-                  description,
-                  style: AppTextstyle.bodyTextStyle(
-                      color: AppColors.grey, overflow: TextOverflow.clip),
-                ),
-              ),
+              16.spaceX,
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    title,
+                    style: AppTextstyle.headingTextStyle(
+                        fontSize: 16.sp,
+                        color: AppColors.white,
+                        fontWeight: FontWeight.w600),
+                  ),
+                  SizedBox(
+                    width: 198.w,
+                    child: Text(
+                      description,
+                      style: AppTextstyle.bodyTextStyle(
+                          color: AppColors.grey, overflow: TextOverflow.clip),
+                    ),
+                  ),
+                ],
+              )
             ],
-          )
+          ),
         ],
       ),
     );
